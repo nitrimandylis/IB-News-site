9bdcd14b65b6e429ca162cafc5c93417d1495d93
supergioo0 <158353879+supergioo0@users.noreply.github.com>
2025-10-10 20:38:44 +0300

    Added function to wait for DOMContent to be loaded

--- static/script.js
+++ static/script.js
@@ -15,20 +15,13 @@ function updateProgressBar() {
 // Add event listener for scroll events
 document.addEventListener('scroll', updateProgressBar);
 
-var burgerMenu = document.getElementById('burger-menu');
+document.addEventListener("DOMContentLoaded", function() {
+  var burgerMenu = document.getElementById('burger-menu');
+  var overlay = document.getElementById('menu');
 
-var overlay = document.getElementById('menu');
-
-burgerMenu.addEventListener('click', function() {
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
+  burgerMenu.addEventListener('click', function() {
+    this.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+  });
 });
 
-var burgerMenu = document.getElementById('burger-menu');
-
-var overlay = document.getElementById('menu');
-
-burgerMenu.addEventListener('click', function() {
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
-});
