018b531a3aa16ef789dbd01e81b08276fe6ccf44
supergioo0 <158353879+supergioo0@users.noreply.github.com>
2025-10-10 20:27:57 +0300

    Adding Functionality Burger Menu

--- static/script.js
+++ static/script.js
@@ -23,3 +23,12 @@ burgerMenu.addEventListener('click', function() {
   this.classList.toggle("close");
   overlay.classList.toggle("overlay");
 });
+
+var burgerMenu = document.getElementById('burger-menu');
+
+var overlay = document.getElementById('menu');
+
+burgerMenu.addEventListener('click', function() {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
